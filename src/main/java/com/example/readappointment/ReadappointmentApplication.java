d9c89dd4e867335731388d3e56a29a49c2894816
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-27 20:45:59 -0500

    :tada: initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/com/example/readappointment/ReadappointmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.readappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadappointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadappointmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/readappointment/ReadappointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadappointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
