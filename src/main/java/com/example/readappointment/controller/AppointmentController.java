39a11b5c5163fc9d289479ec0505c827c9ee6d95
DavidSebas20 <davidsebastian20@outlook.com>
2025-02-14 07:15:43 -0500

    ✨ update request mapping in AppointmentController for consistency

--- src/main/java/com/example/readappointment/controller/AppointmentController.java
+++ src/main/java/com/example/readappointment/controller/AppointmentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/read-appointments/appointments")
+@RequestMapping("/read-appointment/appointments")
 public class AppointmentController {
 
     @Autowired
